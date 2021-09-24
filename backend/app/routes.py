fa66ef453ebcdc9cbeeeeb3b3ff9cb6d9153758d
Gisele Perez Paxtian <leopoldoppaxtian@gmail.com>
2021-09-24 03:31:25 -0500

    Importación del módulo render_template. Creación de carpeta templates junto con archivo index.html, todo en la forma más básica.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,5 +1,8 @@
+from flask import render_template
 from app import app
+
+xyz="El render funciona. Éste párrafo es renderizado y la condición fue procesada por el mismo motor"
 
 @app.route('/')
 def home():
-    return "Hola desde Flask"
+    return render_template("index.html", some_text=xyz)
